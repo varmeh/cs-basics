7b49c903dbeb583040ff70dd2ef03f97933727f5
varmeh <varun.sagi20@gmail.com>
2024-04-03 17:13:43 +0530

    comments in graphs

--- ds/graphs/adjacency-list.js
+++ ds/graphs/adjacency-list.js
@@ -6,12 +6,19 @@ class Graph {
         this.adjacencyList = {}
     }
 
+    /**
+     * Add a new vertex. This vertex will have 0 edge in the beginning
+     */
     addVertex(vertex) {
         if (vertex in this.adjacencyList) return false
         this.adjacencyList[vertex] = []
         return true
     }
 
+    /**
+     * Add edge between 2 vertices
+     * This works only if both vertices exists
+     */
     addEdge(v1, v2) {
         // Both vertex exists
         if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
@@ -22,6 +29,9 @@ class Graph {
         return false
     }
 
+    /**
+     * Remove Edge between vertices v1 & v2
+     */
     removeEdge(v1, v2) {
         if (this.adjacencyList[v1]) {
             this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
@@ -32,6 +42,11 @@ class Graph {
         }
     }
 
+    /**
+     * Remove Vertex v
+     * - Ensure all Edges to vertex v are also removed
+     * - Which means removing v entry from other vertices list
+     */
     removeVertex(v) {
         // 1st remove edges of vertex v with other vertices
         for (let vertex of this.adjacencyList[v]) {
