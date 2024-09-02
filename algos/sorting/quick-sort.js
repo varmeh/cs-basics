070c1b60cc2770ab4b1d722eccfea04eaf9147f2
varmeh <varun.sagi20@gmail.com>
2024-09-02 13:02:38 +0530

    edits

--- algos/sorting/quick-sort.js
+++ algos/sorting/quick-sort.js
@@ -4,21 +4,20 @@
  * Not Recommended as Time Complexity is O(n^2) in case of sorted array.
  * For sorted Array, there will be no left side of the pivot. So, the time complexity will be O(n^2)
  */
-function pivotStart(arr, start, end) {
+function pivotStart(arr, startIndex, endIndex) {
     const swap = (i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]])
-    let pivot = arr[start]
-    let swapIndex = start // Index of the smaller elements
-    for (let i = start + 1; i <= end; i++) {
+    let pivot = arr[startIndex]
+    let noSmallerThanPivot = startIndex // Index of the smaller elements
+    for (let i = startIndex + 1; i <= endIndex; i++) {
         if (arr[i] < pivot) {
-            swapIndex++
-            swap(swapIndex, i)
+            noSmallerThanPivot++
+            swap(startIndex + noSmallerThanPivot, i)
         }
     }
 
-    // Swap the pivot from the start to the swapIndex
-    // Value at SwapIndex < Pivot. So, swap ensures that all values less than pivot are to the left of swapIndex
-    swap(start, swapIndex)
-    return swapIndex
+    // Pivot at position - startIndex, last number smaller than it at position - noSmallerThanPivot - So, swap
+    swap(startIndex, startIndex + noSmallerThanPivot)
+    return noSmallerThanPivot
 }
 
 // console.log(pivot([20, 35, 5, 15, 30], 0, 4))
@@ -30,61 +29,61 @@ console.log(pivotStart([5, 4, 3, 2, 1], 0, 4))
  * Here, we randomly select a pivot & replace it with the 1st element of the array
  * Pivot === 1st element of the array
  */
-function pivotRandomized(arr, start, end) {
+function pivotRandomized(arr, startIndex, endIndex) {
     const swap = (i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]])
 
     // Randomly select a pivot & replace it with the 1st element of the array
-    const pivotIndex = Math.floor(Math.random() * (end - start + 1) + start)
-    swap(start, pivotIndex)
+    const pivotIndex = Math.floor(Math.random() * (endIndex - startIndex + 1) + startIndex)
+    swap(startIndex, pivotIndex)
 
     // Start the pivot from the 1st element of the array
-    let pivot = arr[start]
-    let swapIndex = start // Index of the smaller elements
-    for (let i = start + 1; i <= end; i++) {
+    let pivot = arr[startIndex]
+    let noSmallerThanPivot = startIndex // Index of the smaller elements
+    for (let i = startIndex + 1; i <= endIndex; i++) {
         if (arr[i] < pivot) {
-            swapIndex++
-            swap(swapIndex, i)
+            noSmallerThanPivot++
+            swap(noSmallerThanPivot, i)
         }
     }
 
-    // Swap the pivot from the start to the swapIndex
-    // Value at SwapIndex < Pivot. So, swap ensures that all values less than pivot are to the left of swapIndex
-    swap(start, swapIndex)
-    return swapIndex
+    // Swap the pivot from the startIndex to the noSmallerThanPivot
+    // Value at SwapIndex < Pivot. So, swap ensures that all values less than pivot are to the left of noSmallerThanPivot
+    swap(startIndex, noSmallerThanPivot)
+    return noSmallerThanPivot
 }
 
 /**
  * Pivot === mid element of the array
  * Not recommended. But, good to know
  */
-function pivotMid(arr, start, end) {
+function pivotMid(arr, startIndex, endIndex) {
     const swap = (i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]])
-    const mid = Math.floor((start + end) / 2)
+    const mid = Math.floor((startIndex + endIndex) / 2)
     const pivot = arr[mid]
-    let swapIndex = start
-    for (let i = start; i <= end; i++) {
+    let noSmallerThanPivot = startIndex
+    for (let i = startIndex; i <= endIndex; i++) {
         if (arr[i] < pivot) {
-            swap(swapIndex, i)
-            swapIndex++
+            swap(noSmallerThanPivot, i)
+            noSmallerThanPivot++
         }
     }
 
-    // Swap the pivot from the start to the swapIndex
-    // Value at SwapIndex < Pivot. So, swap ensures that all values less than pivot are to the left of swapIndex
-    swap(mid, swapIndex)
-    return swapIndex
+    // Swap the pivot from the startIndex to the noSmallerThanPivot
+    // Value at SwapIndex < Pivot. So, swap ensures that all values less than pivot are to the left of noSmallerThanPivot
+    swap(mid, noSmallerThanPivot)
+    return noSmallerThanPivot
 }
 
 // console.log(pivotMid([20, 35, 28, 15, 30], 0, 4))
 
-function quicksort(arr, start = 0, end = arr.length - 1) {
-    if (start >= end) return // Base case - when the array has 1 element or less
-    const pivotIndex = pivotRandomized(arr, start, end)
+function quicksort(arr, startIndex = 0, endIndex = arr.length - 1) {
+    if (startIndex >= endIndex) return // Base case - when the array has 1 element or less
+    const pivotIndex = pivotRandomized(arr, startIndex, endIndex)
     // left
-    quicksort(arr, start, pivotIndex - 1)
+    quicksort(arr, startIndex, pivotIndex - 1)
 
     // right
-    quicksort(arr, pivotIndex + 1, end)
+    quicksort(arr, pivotIndex + 1, endIndex)
 }
 
 const arr = [20, 35, 5, 15, 30]
